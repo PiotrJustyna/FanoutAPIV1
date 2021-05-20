e812b502f4a41a58b729a8fb6349059ae60e36dd
Habeeb Odunsi <habeeb.odunsi@lexisnexisrisk.com>
2021-05-20 14:21:14 +0100

    add metrics server

--- dockerfile
+++ dockerfile
@@ -1,7 +1,6 @@
 
 FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
 WORKDIR /app
-EXPOSE 5432
 
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
 WORKDIR /src
---
+++ src/Middlewares/MetricsMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Prometheus;
+
+public class MetricsMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
+
+    public MetricsMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+    {
+        _next = next;
+        _logger = loggerFactory.CreateLogger<MetricsMiddleware>();
+        
+    }
+    
+    public async Task Invoke(HttpContext httpContext)  
+    {  
+        var path = httpContext.Request.Path.Value;  
+        var method = httpContext.Request.Method;  
+  
+        var counter = Metrics.CreateCounter("request_count", "Total http Requests", new CounterConfiguration  
+        {  
+            LabelNames = new[] { "path", "method", "status" }  
+        });
+
+        int statusCode; 
+  
+        try  
+        {  
+            await _next.Invoke(httpContext);  
+        }  
+        catch (Exception)  
+        {  
+            statusCode = 500;  
+            counter.Labels(path ?? string.Empty, method, statusCode.ToString()).Inc();  
+  
+            throw;  
+        }  
+          
+        if (path != "/metrics")  
+        {  
+            statusCode = httpContext.Response.StatusCode;  
+            counter.Labels(path ?? string.Empty, method, statusCode.ToString()).Inc();  
+        }  
+    }  
+}
+
+
+public static class RequestMiddlewareExtensions
+{
+    public static IApplicationBuilder UseRequestMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<MetricsMiddleware>();
+    }
+}
